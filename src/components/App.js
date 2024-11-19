5bbc4a0c73975a5933b3bc1b13e9527505c534e9
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-11-19 12:21:24 +0300

    lab work

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import blogData from "../data/blog";
-
+import Header from "./Header";
+import ArticleList from "./ArticleList";
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* Header component */}
+      <Header name={blogData.name} />
+
+      {/* Sidebar */}
+      <aside>
+        <p>About this blog: {blogData.about}</p>
+        <img src={blogData.image} alt="blog logo" />
+      </aside>
+
+      {/* Main content */}
+      <main>
+        <ArticleList posts={blogData.posts} />
+      </main>
     </div>
   );
 }
